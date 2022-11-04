36dd601d56f61fbe5243d21b342c5bdb4236a1b9
Tabithadejong <tabithadejong654@gmail.com>
2022-11-04 13:34:05 +0000

    Make one function that updates all spreadsheets

--- run.py
+++ run.py
@@ -58,23 +58,12 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data): 
-    """
-    Update sales worksheet, add new row with the list provided data 
-    """
-    print("Updating sales worksheet....\n")
-    sales_worksheet = SHEET.worksheet("sales") 
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated succesfully")
+def update_worksheet(data, worksheet): 
 
-def update_surplus_worksheet(data): 
-    """
-    Update worksheet surplus with data from calculation
-    """
-    print("Updating surplus worksheet....\n")
-    surplus_worksheet = SHEET.worksheet("surplus") 
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated succesfully")
+    print(f"Updating {worksheet} worksheet....\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} is updated with{data} succesfully\n")
 
 
 def calculate_surplus_data(sales_row): 
@@ -114,9 +103,9 @@ def main():
 
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation")
